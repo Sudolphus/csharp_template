1018cba02073e44bf8b86c51a8c7d43cea098a3e
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-19 16:56:04 -0700

    update template for MVC

---
+++ ProjectName/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using ProjectName.Models;
+
+namespace ProjectName.Controllers
+{
+  public class HomeController : Controller
+  {
+    [Route("/")]
+    public ActionResult Homepage() {
+      return View();
+    }
+  }
+}
