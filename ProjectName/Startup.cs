8bebc8519e6f1443f0be4762d36e4520b5e2ff24
Micheal Hansen <micheal.hansen@gmail.com>
2020-07-19 16:57:38 -0700

    fix typo in template

--- ProjectName/Startup.cs
+++ ProjectName/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
-namespace ProgramName
+namespace ProjectName
 {
   public class Startup
   {
